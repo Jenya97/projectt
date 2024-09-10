cf40650259cf9f89e3a6d286790fb02646bc63d3
Jenya97 <grigoryan_jenya@bk.ru>
2024-09-10 23:22:53 +0400

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,25 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+  images: {
+    unoptimized: true,
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+};
+
+export default nextConfig;
